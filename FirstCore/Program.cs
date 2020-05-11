2df87cf3df404d3dba1303daaec0e0df85f211f1
ParasGupta13 <54528036+ParasGupta13@users.noreply.github.com>
2020-05-12 00:27:47 +0530

    test2

--- FirstCore/Program.cs
+++ FirstCore/Program.cs
@@ -19,6 +19,6 @@ namespace FirstCore
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();  //change by paras gupta
+                .UseStartup<Startup>();  //change by paras gupta on 12-05-2020
     }
 }
