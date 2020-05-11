be11bcdb3d27c62453ea8dd69823a99295cf640c
Paras Gupta <parasgupta100.pg@gmail.com>
2020-05-11 23:56:21 +0530

    paras 2

--- FirstCore/Program.cs
+++ FirstCore/Program.cs
@@ -19,6 +19,6 @@ namespace FirstCore
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>();  //change by paras
     }
 }
